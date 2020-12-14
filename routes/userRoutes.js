265bfff669d67a5a18e5b194a8b999c7f5fee341
KeithMarex <koenvandermarel@gmail.com>
2020-12-14 21:52:36 +0100

    Fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,19 +5,13 @@ const userController = require('../controllers/userController');
 // GET /user/all
 router.get('/all', userController.getAllUsers);
 
-// GET /user/get/:email
-router.get('/get/:email', userController.getUser);
-
 // POST /user/create/
 router.post('/create', userController.createUser);
 
-// DELETE /user/delete
-router.post('/delete', userController.deleteUser);
-
-// DELETE /user/checkUserLogin
+// POST /user/checkUserLogin
 router.get('/checkUserLogin/:useremail/:userpassword', userController.checkUserLogin);
 
 // POST /user/password/change
-router.post('/password/change', userController.changePassword);
+// router.post('/password/change', userController.changePassword);
 
 module.exports = router;
