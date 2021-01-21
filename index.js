7e549ab3f26a558c35064210e167bfc768ae828f
KoennieTime <koenvandermarel@gmail.com>
2021-01-21 19:12:07 +0100

    Naja dan maar gewoon weer terug naar het begin

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Make use of libraries
 const fs = require('fs');
+const http = require('http');
 const https = require('https');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const HTTPport = 80;
 const HTTPSport = 443;
 
 var privateKey  = fs.readFileSync('/etc/letsencrypt/live/iprwc.kvdmr.nl/privkey.pem');
@@ -16,8 +18,17 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const orderRoutes = require('./routes/orderRoutes');
-const { use } = require('./routes/userRoutes');
 
+/**
+ * Use body parser for all handlers
+ */
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 
 /**
  * Create all base paths for the api
@@ -28,31 +39,27 @@ app.use('/cart', cartRoutes);
 app.use('/order', orderRoutes);
 
 /**
- * Use body parser for all handlers
+ * Log all base information for the api
  */
-var corsOptions = {
-    origin: 'https://iprwcshop.kvdmr.nl',
-    optionsSuccessStatus: 200 // For legacy browser support
-}
-
-app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://iprwcshop.kvdmr.nl');
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({message: message, data: data});
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-})
-app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-);
+});
 
 /**
  * Write the port number in the console window
  */
+var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(HTTPport, () => {
+    console.log(`[API Controller] App running on HTTP port ${HTTPport}.`)});
 
 httpsServer.listen(HTTPSport, () => {
     console.log(`[API Controller] App running on HTTPS port ${HTTPSport}.`)});
