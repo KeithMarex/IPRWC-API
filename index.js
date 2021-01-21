1465f8de42c17a72d909b9c4400916b0e319dd4f
KoennieTime <koenvandermarel@gmail.com>
2021-01-21 17:54:12 +0100

    Small fix

--- index.js
+++ index.js
@@ -23,19 +23,9 @@ const orderRoutes = require('./routes/orderRoutes');
 /**
  * Use body parser for all handlers
  */
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://iprwcshop.kvdmr.nl'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.header('Access-Control-Allow-Credentials', 'true');
-    }
-    return next();
-});
-
-app.use(cors());
+app.use(cors({
+    origin: 'https://iprwcshop.kvdmr.nl'
+  }));
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
